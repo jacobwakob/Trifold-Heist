f7ccc3db4dc9bce141606c374a72386be61f5189
1-Wammy-1 <cammywammy5249@outlook.com>
2025-11-10 21:34:21 +1030

    random push

---
+++ The Game/Assets/Scripts/UI/GoldPickup1.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoldPickup1 : MonoBehaviour
+{
+    public string InteractMessage => ObjectInteractMessage;
+
+    [SerializeField] protected string ObjectInteractMessage;
+
+    void ChangeScene()
+    {
+        //UnityEngine.Cursor.lockState = CursorLockMode.None;
+        //UnityEngine.Cursor.visible = true;
+        SceneManager.LoadScene(9);
+    }
+
+    public void Interact()
+    {
+        ChangeScene();
+    }
+}
+
